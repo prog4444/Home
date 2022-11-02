ef559dd2b6731e33ccde1c38d7e3d6e88e206e4e
Rasul <falcon.9roc@gmail.com>
2022-11-02 16:07:35 +0500

    davomash hay

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,3 +19,6 @@ use Illuminate\Support\Facades\Route;
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 
+Route::middleware("auth:api")->group(function(){
+    Route::get('/show', [UserController::class, 'show']);
+});
